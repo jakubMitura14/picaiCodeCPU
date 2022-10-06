fa8781cb9087439a6acc1ec5ebca89a50476479c
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-10-06 15:00:59 +0200

    up

--- Three_chan_baseline.py
+++ Three_chan_baseline.py
@@ -65,10 +65,15 @@ def loadLib(name,path):
 # manageMetaData =loadLib("ManageMetadata", "/mnt/disks/sdb/piCaiCode/preprocessing/ManageMetadata.py")
 # dataUtils =loadLib("dataUtils", "/mnt/disks/sdb/piCaiCode/dataManag/utils/dataUtils.py")
 
-unets =loadLib("unets", "/mnt/disks/sdb/piCaiCode/model/unets.py")
-DataModule =loadLib("DataModule", "/mnt/disks/sdb/piCaiCode/model/DataModule.py")
-LigtningModel =loadLib("LigtningModel", "/mnt/disks/sdb/piCaiCode/model/LigtningModel.py")
-semisuperPreprosess =loadLib("semisuperPreprosess", "/mnt/disks/sdb/piCaiCode/preprocessing/semisuperPreprosess.py")
+# unets =loadLib("unets", "/mnt/disks/sdb/piCaiCode/model/unets.py")
+# DataModule =loadLib("DataModule", "/mnt/disks/sdb/piCaiCode/model/DataModule.py")
+# LigtningModel =loadLib("LigtningModel", "/mnt/disks/sdb/piCaiCode/model/LigtningModel.py")
+# semisuperPreprosess =loadLib("semisuperPreprosess", "/mnt/disks/sdb/piCaiCode/preprocessing/semisuperPreprosess.py")
+
+import preprocessing.ManageMetadata as manageMetaData
+import model.unets as unets
+import model.DataModule as DataModule
+import model.LigtningModel as LigtningModel
 
 
 
