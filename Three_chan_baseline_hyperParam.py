e651f0e24a631b87b87e3699cdb585c84d3602b9
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-10-06 15:27:09 +0200

    up

--- Three_chan_baseline_hyperParam.py
+++ Three_chan_baseline_hyperParam.py
@@ -247,9 +247,9 @@ study = optuna.create_study(
         study_name=experiment_name
         ,sampler=optuna.samplers.NSGAIISampler()    
         ,pruner=optuna.pruners.HyperbandPruner()
-        #,storage="mysql://root@34.147.7.30:3306/picai_hp_35"
+        ,storage="mysql://root@34.147.7.30:3306/picai_hp_36"
         ,load_if_exists=True
-        ,storage="mysql://root@127.0.0.1:3306/picai_hp_35"
+        #,storage="mysql://root@127.0.0.1:3306/picai_hp_35"
         )
         #mysql://root@localhost/example
 study.optimize(objective, n_trials=40)
