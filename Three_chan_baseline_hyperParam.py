34f65d76d00f221a2847ec188b2ec6d8f90e7045
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-10-06 14:45:35 +0200

    up

--- Three_chan_baseline_hyperParam.py
+++ Three_chan_baseline_hyperParam.py
@@ -50,9 +50,7 @@ import sys
 from pytorch_lightning.loggers import TensorBoardLogger
 import optuna
 from optuna.integration import PyTorchLightningPruningCallback
-# from ray import air, tune
-# from ray.air import session
-# from ray.tune import CLIReporter
+
 from optuna.visualization import plot_contour
 from optuna.visualization import plot_edf
 from optuna.visualization import plot_intermediate_values
@@ -60,7 +58,6 @@ from optuna.visualization import plot_optimization_history
 from optuna.visualization import plot_parallel_coordinate
 from optuna.visualization import plot_param_importances
 from optuna.visualization import plot_slice
-# torch.multiprocessing.freeze_support()
 
 def loadLib(name,path):
     spec = importlib.util.spec_from_file_location(name, path)
